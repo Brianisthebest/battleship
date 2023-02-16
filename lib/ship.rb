9b52b3b10f668998094186e79200c8106665ea23
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-16 15:54:06 -0700

    Refactor: fixed sunk method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,14 +1,17 @@
 class Ship
-attr_reader :name, :length, :health, :sunk
+attr_reader :name, :length, :health
   def initialize(name, length)
     @name = name
     @length = length
     @health = 3
-    @sunk = false
   end
 
   def sunk?
-    @sunk
+    if @health == 0
+      true
+    else
+      false
+    end
   end
 
   def hit
