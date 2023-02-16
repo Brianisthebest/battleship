b3de03c48b2f1f1378e02f57b9896e64fb2ee8dc
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-16 15:47:34 -0700

    Feat: add ship methods and definitions

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,6 +1,17 @@
 class Ship
+attr_reader :name, :length, :health, :sunk
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health = 3
+    @sunk = false
+  end
 
-  def initialize
-    
+  def sunk?
+    @sunk
+  end
+
+  def hit
+    @health -= 1
   end
 end
