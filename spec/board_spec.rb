52f0c12b2622c2def42939bfc88be6971c70e54a
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-18 12:18:53 -0700

    test: Add test for hash of cells and valid_coordinate

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,6 +15,24 @@ RSpec.describe Board do
 
     it 'is a hash of cells' do
       expect(@board.cells).to be_a(Hash)
+      expect(@board.cells.keys.count).to eq(16)
+      expect(@board.cells.values).to be_a(Array)
+      expect(@board.cells.values.count).to eq(16)
+      expect(@board.cells.values.first).to be_a(Cell)
     end
+
+  describe '#valid_coordinate?' do
+    it 'can determine if a coordinate is valid or not' do
+      expect(@board.valid_coordinate?('A1')).to eq(true)
+      expect(@board.valid_coordinate?('D4')).to eq(true)
+      expect(@board.valid_coordinate?('A5')).to eq(false)
+      expect(@board.valid_coordinate?('E1')).to eq(false)
+      expect(@board.valid_coordinate?('A22')).to eq(false)
+    end
+
+  
+  end
+
+
   end
 end
