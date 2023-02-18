09e033137cf859ee1aac72778dbae8a43f13bee6
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-18 12:18:08 -0700

    feat: Add valid_coordinate? method

--- lib/board.rb
+++ lib/board.rb
@@ -20,6 +20,11 @@ class Board
       "D3" => Cell.new("D3"),
       "D4" => Cell.new("D4"),
     }
-    
   end
+
+  def valid_coordinate?(coordinate)
+    @cells.keys.include?(coordinate)
+  end
+
+
 end
