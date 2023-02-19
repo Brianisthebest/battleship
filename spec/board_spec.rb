c130411c9be88f19a39e32f0b4e52027d5844889
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-18 19:24:36 -0700

    test: Add test for place method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -58,4 +58,15 @@ RSpec.describe Board do
       expect(@board.valid_placement?(@cruiser, ['B1', 'C1', 'D1'])).to eq(true)
     end
   end
+
+  describe '#place' do
+    it 'can place a ship on the board' do
+      @board.place(@cruiser, ['A1', 'A2', 'A3'])
+      @cell_1 = @board.cells["A1"]
+      @cell_2 = @board.cells["A2"]
+      @cell_3 = @board.cells["A3"]
+      @cell_1.ship
+     
+    end
+  end
 end
