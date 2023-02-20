9c69dbb1711ccdc97bff40160b77de4e0309f7be
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-20 14:14:46 -0700

    Tests: add test for render method to render board and show ship placement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -97,7 +97,15 @@ RSpec.describe Board do
 
   describe '#render' do
     it 'can render the board' do
-      @board.render
+      expect(@board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+    end
+
+    it 'can check for player ships, hits, and misses' do
+      @board.place(@cruiser, ['A1', 'A2', 'A3'])
+
+      expect(@board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+      expect(@board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
+
     end
   end
 end
