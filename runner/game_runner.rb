c7c1bd2efa2486f5658ac8017fc6bcb0d7213235
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-21 14:14:52 -0700

    add new methods

--- runner/game_runner.rb
+++ runner/game_runner.rb
@@ -7,6 +7,16 @@ game = Game.new
 
 puts game.main_menu
 
-# puts game.comp_ship_placement
+puts game.comp_ship_placement
 
 puts game.player_ship_placement
+
+puts game.display_boards
+
+puts game.player_turn
+
+puts game.display_boards
+
+puts game.comp_turn
+
+puts game.display_boards
