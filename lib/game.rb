51d58704d33ad60ed89631068c1f982b0f2a1eba
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-21 14:14:33 -0700

    feat: Add player and comp turns method and display board method

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,15 @@
 class Game
-attr_reader :main_menu
+  attr_reader :main_menu
   def initialize
     @player_board = Board.new
     @comp_board  = Board.new
   end
 
+  
+
+
+
+  
   def main_menu
     puts 'Welcome to BATTLESHIP
     Enter p to play. Enter q to quit.'
@@ -25,9 +30,12 @@ attr_reader :main_menu
 
   def comp_ship_placement
     cruiser = Ship.new('Cruiser', 3)
-      random_placement = @comp_board.cells.keys.sample(3)
-      #  if @comp_board.valid_coordinate?(random_placement)
+    submarine = Ship.new('Submarine', 3)
+    coordinates = @comp_board.cells.keys.each_cons(3).to_a.sample 
+    if @comp_board.valid_placement?(cruiser, coordinates)
+        @comp_board.place(cruiser, coordinates)
         #  @comp_board.place(cruiser, ['A1', 'A2', 'A3']) ##we just set computer placement coordinates. figure out randomize later
+    end
   end
 
   def player_ship_placement
@@ -47,8 +55,31 @@ attr_reader :main_menu
       puts 'Invalid response.'
       self.player_ship_placement
     end
-     self.player_submarine
+      self.player_submarine
+      puts "***********************************"
   end
+
+  def display_boards
+    puts "***********Computer Board*************************"
+    puts @comp_board.render
+    puts "***********Player Board*************************"
+    puts @player_board.render(true)
+  end
+
+  def player_turn
+    self.display_boards
+    puts "Choose a vailid coordinate to fire upon"
+    coordinate = gets.chomp
+    if @comp_board.valid_coordinate?(coordinate) 
+      @comp_board.cells[coordinate].fire_upon_empty ###fire upon method not working. this will return miss every time thoguh
+    end
+  end
+
+  def comp_turn
+    comp_guess = @comp_board.cells.keys.sample(1)
+    @player_board.cells[comp_guess].fire_upon
+  end
+
   
   #helper method
   def player_submarine
