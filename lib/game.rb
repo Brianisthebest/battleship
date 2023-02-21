ba95b1a8c7466525f247801612c91582d80278df
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-20 19:34:36 -0700

    feat: Add player ship placement and player submarine methods

--- lib/game.rb
+++ lib/game.rb
@@ -15,7 +15,8 @@ attr_reader :main_menu
       puts 'Thanks for playing, yall come back now.'
     elsif input == 'p'
       puts 'To your battle station!'
-      comp_ship_placement
+      # self.player_ship_placement
+      # self.comp_ship_placement
     else
       puts 'Invalid response.'
       self.main_menu
@@ -24,8 +25,43 @@ attr_reader :main_menu
 
   def comp_ship_placement
     cruiser = Ship.new('Cruiser', 3)
-    rand_coords = @valid_vertical
-    @comp_board.place(cruiser, rand_coords.sample(3)) 
-      # @comp_board.cells.keys.sample(3)
+      random_placement = @comp_board.cells.keys.sample(3)
+      #  if @comp_board.valid_coordinate?(random_placement)
+        #  @comp_board.place(cruiser, ['A1', 'A2', 'A3']) ##we just set computer placement coordinates. figure out randomize later
+  end
+
+  def player_ship_placement
+    cruiser = Ship.new('Cruiser', 3)
+
+    puts "It's time to put out your ships."
+    puts "The cruiser is 3 units long, the submarine is 2 units long"
+    puts "***********************************"
+    puts @player_board.render
+    puts "Enter the squares for cruiser (3 spaces):"
+    coordinates = gets.chomp.split
+    if @player_board.valid_placement?(cruiser, coordinates)
+      @player_board.place(cruiser, coordinates)
+      puts "***********************************"
+      puts @player_board.render(true)
+    else
+      puts 'Invalid response.'
+      self.player_ship_placement
+    end
+     self.player_submarine
+  end
+  
+  #helper method
+  def player_submarine
+    submarine = Ship.new('Submarine', 2)
+    puts 'Enter the squares for submarine (2 spaces):'
+    coordinates = gets.chomp.split
+    if @player_board.valid_placement?(submarine, coordinates)
+     @player_board.place(submarine, coordinates)
+     puts "***********************************"
+     puts @player_board.render(true)
+    else
+     puts 'Invalid response.'
+     self.player_submarine
+    end
   end
 end
