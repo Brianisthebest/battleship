7f64fc732043087e01440ebbd268ca87bd8b6289
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-22 08:13:05 -0700

    Add instance variables for valid ship placements and plug them into valid placement method

--- lib/board.rb
+++ lib/board.rb
@@ -20,7 +20,6 @@ class Board
       "D3" => Cell.new("D3"),
       "D4" => Cell.new("D4"),
     }
-    @valid_vertical = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2', 'A3', 'B3', 'C3', 'D3', 'A4', 'B4', 'C4', 'D4']
     @valid_submarine = [['A1', 'A2'], ['A2', 'A3'], ['A3', 'A4'], ['B1', 'B2'], ['B2', 'B3'], ['B3', 'B4'], ['C1', 'C2'], ['C2', 'C3'], ['C3', 'C4'], ['D1', 'D2'], ['D2', 'D3'], ['D3', 'D4'], ['A1', 'B1'], ['A2', 'B2'], ['A3', 'B3'], ['A4', 'B4'], ['B1', 'C1'], ['B2', 'C2'], ['B3', 'C3'], ['B4', 'C4'], ['C1', 'D1'], ['C2', 'D2'], ['C3', 'D3'], ['C4', 'D4']]
 
     @valid_cruiser = [['A1', 'A2', 'A3'], ['A2', 'A3', 'A4'], ['B1', 'B2', 'B3'], ['B2', 'B3', 'B4'], ['C1', 'C2', 'C3'], ['C2', 'C3', 'C4'], ['D1', 'D2', 'D3'], ['D2', 'D3', 'D4'], ['A1', 'B1', 'C1'], ['B1', 'C1', 'D1'], ['A2', 'B2', 'C2'], ['B2', 'C2', 'D2'], ['A3', 'B3', 'C3'], ['B3', 'C3', 'D3'], ['A4', 'B4', 'C4'], ['B4', 'C4', 'D4']]
@@ -73,24 +72,6 @@ end
     end
   end
   
-  # def consecutive_coordinates(ship, coordinates)
-  #   @cells.keys.each_cons(ship.length) do |key|
-  #     if key == coordinates
-  #       return true
-  #     end
-  #   end
-  #   false
-  # end
-
-  # def valid_verticals(ship, coordinates)
-  #   @valid_vertical.each_cons(ship.length) do |key|
-  #     if key == coordinates
-  #       return true
-  #     end
-  #   end
-  #   false
-  # end
-
   def valid_submarine(ship, coordinates)
   @valid_submarine.select do |key|
     if key == coordinates
